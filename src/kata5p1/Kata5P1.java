535c7190196c323673760ded8bbe6474942aa90c
G10Mini <G10Mini@DESKTOP-4RFEI7P>
2021-12-30 03:58:01 +0100

    Kata5P1 v2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,10 +1,30 @@
 package kata5p1;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Kata5P1 {
 
     public static void main(String[] args) {
-        SelectApp app = new SelectApp();
-        app.selectAll();
+        createTable();
     }
     
+    public static void createTable() {
+        
+        String url = "jdbc:sqlite:KATA5.db";
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " dir text NOT NULL);";
+        
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement sm = conn.createStatement();
+            sm.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
