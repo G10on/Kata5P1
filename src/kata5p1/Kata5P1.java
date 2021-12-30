55335b32a9190d078c375b584e530031a2fb3779
G10Mini <G10Mini@DESKTOP-4RFEI7P>
2021-12-30 04:23:34 +0100

    Kata5P1 v3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,28 +3,38 @@ package kata5p1;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
+import static kata5p1.MailListReader.read;
 
 public class Kata5P1 {
 
+    private static String filename = "email.txt";
+
     public static void main(String[] args) {
-        createTable();
+        Connection connection = connect("KATA5.db");
+        String sql = "INSERT INTO EMAIL(dir) VALUES(?)";
+        List<String> readMails = read(filename);
+        for (String email : readMails) {
+            try {
+                PreparedStatement pstmt = connection.prepareStatement(sql);
+                pstmt.setString(1, email);
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
-    
-    public static void createTable() {
-        
-        String url = "jdbc:sqlite:KATA5.db";
-        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
-                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
-                + " dir text NOT NULL);";
-        
+
+    private static Connection connect(String dbName) {
+        String url = "jdbc:sqlite:" + dbName;
+        Connection con = null;
         try {
-            Connection conn = DriverManager.getConnection(url);
-            Statement sm = conn.createStatement();
-            sm.execute(sql);
-            System.out.println("Tabla creada");
+            con = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return con;
     }
 }
